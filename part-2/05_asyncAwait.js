18172211f168d807232222cc4b76a44224076fea
UnicusStella <unicusstella@gmail.com>
2021-07-27 15:44:21 +0900

    modify

--- part-2/05_asyncAwait.js
+++ part-2/05_asyncAwait.js
@@ -4,7 +4,7 @@ const { getDataFromFilePromise } = require('./02_promiseConstructor');
 const user1Path = path.join(__dirname, 'files/user1.json');
 const user2Path = path.join(__dirname, 'files/user2.json');
 
-const readAllUsersAsyncAwait = () => {
+const readAllUsersAsyncAwait = async () => {
   // TODO: async/await 키워드를 이용해 작성합니다
   let newArr = [];
   let x = await getDataFromFilePromise(user1Path).then((data) =>
@@ -19,7 +19,7 @@ const readAllUsersAsyncAwait = () => {
   return newArr;
 };
 
-// readAllUsersAsyncAwait();
+readAllUsersAsyncAwait();
 
 module.exports = {
   readAllUsersAsyncAwait,
