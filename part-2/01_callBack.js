baf9b2c1881a061518fc439bf95b710abd4427e7
UnicusStella <unicusstella@gmail.com>
2021-07-27 13:36:09 +0900

    callback

--- part-2/01_callBack.js
+++ part-2/01_callBack.js
@@ -2,6 +2,12 @@ const fs = require('fs');
 
 const getDataFromFile = function (filePath, callback) {
   // TODO: fs.readFile을 이용해 작성합니다
+  fs.readFile(filePath, (err, data) => {
+    if (err) callback(err, data);
+    else {
+      callback(data, err);
+    }
+  });
 };
 
 getDataFromFile('README.md', (err, data) => console.log(data));
