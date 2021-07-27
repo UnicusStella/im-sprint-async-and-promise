45f48168b1a5188a89e18e35b961541e95707491
UnicusStella <unicusstella@gmail.com>
2021-07-27 13:17:45 +0900

    study

--- part-2/01_callBack.js
+++ part-2/01_callBack.js
@@ -2,10 +2,9 @@ const fs = require('fs');
 
 const getDataFromFile = function (filePath, callback) {
   // TODO: fs.readFile을 이용해 작성합니다
-  setTimeout(callback, filePath);
 };
 
-// getDataFromFile('README.md', (err, data) => console.log(data));
+getDataFromFile('README.md', (err, data) => console.log(data));
 
 module.exports = {
   getDataFromFile,
