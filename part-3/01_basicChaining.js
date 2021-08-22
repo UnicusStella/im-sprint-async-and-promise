3285c5903e66488aeb7d4f296a5ad442ead19698
UnicusStella <unicusstella@gmail.com>
2021-08-22 23:42:01 +0900

    part-3 01_basicChaning Complete

--- part-3/01_basicChaining.js
+++ part-3/01_basicChaining.js
@@ -4,6 +4,15 @@ var weatherURL = 'http://localhost:5000/data/weather';
 function getNewsAndWeather() {
   // TODO: fetch을 이용해 작성합니다
   // TODO: 여러개의 Promise를 then으로 연결하여 작성합니다
+  return fetch(newsURL)
+    .then((res) => res.json())
+    .then((data1) => {
+      return fetch(weatherURL)
+        .then((res) => res.json())
+        .then((data2) => {
+          return { news: data1.data, weather: data2 };
+        });
+    });
 }
 
 if (typeof window === 'undefined') {
