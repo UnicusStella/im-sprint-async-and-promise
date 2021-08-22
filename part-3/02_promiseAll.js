d5e034fa4aaca731c7f35831b4c431ece3315cc8
UnicusStella <unicusstella@gmail.com>
2021-08-22 23:48:58 +0900

    part-3 02_promiseAll Complete

--- part-3/02_promiseAll.js
+++ part-3/02_promiseAll.js
@@ -3,6 +3,13 @@ var weatherURL = 'http://localhost:5000/data/weather';
 
 function getNewsAndWeatherAll() {
   // TODO: Promise.all을 이용해 작성합니다
+  return Promise.all([fetch(newsURL), fetch(weatherURL)])
+    .then(([data1, data2]) => {
+      return Promise.all([data1.json(), data2.json()]);
+    })
+    .then(([news, weather]) => {
+      return { news: news.data, weather: weather };
+    });
 }
 
 if (typeof window === 'undefined') {
